c09be797f5a3e113506de2e2489a03a75888c1d8
Armando Carvajal <armandoecr_96@hotmail.com>
2018-02-25 10:48:14 -0600

    Listo estructura general de envio de correos electronicos

--- enviar.php
+++ enviar.php
@@ -1,5 +1,5 @@
+<!-- Inicio de codigo PHP -->
 <?php
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 //C:\xampp\htdocs\vendor\phpmailer\phpmailer\src
@@ -8,6 +8,30 @@ require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
 require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 require 'vendor/autoload.php';
+echo $_POST['name'];
+//variables
+if(isset($_POST["name"])){
+    echo 'Hola, '.$_POST["name"];
+    $nombre = $_POST["name"];
+} else {
+       echo 'No has puesto tu nombre.';
+       }
+       
+if(isset($_POST["email"])){
+    echo 'Hola, '.$_POST["email"];
+    $correo = $_POST["email"];
+} else {
+        echo 'No has puesto tu nombre.';
+        }
+
+if(isset($_POST["texto"])){
+    echo 'Hola, '.$_POST["texto"];
+    $texto = $_POST["texto"];
+} else {
+        echo 'No has puesto tu nombre.';
+        }
+
+
 
 //Configuración de correo electronico
 $mail = new PHPMailer;
@@ -15,7 +39,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com'; //Servidor smtp del correo
 $mail->SMTPAuth = true;
 $mail->Username = 'armandoecr96@gmail.com'; //Correo electronico del remitente
-$mail->Password = '***'; //Constraseña del remitente
+$mail->Password = 'teamoalizeebananera'; //Constraseña del remitente
 
 $mail->SMTPSecure = 'tls'; //Tipo de seguridad
 $mail->Port = 587; //Puerto del servidor smtp
@@ -25,12 +49,14 @@ $mail->Port = 587; //Puerto del servidor smtp
 // Datos del Correo
 
 $mail->SetFrom('armandoecr96@gmail.com', "Nombre Persona"); //Correo electronico del remitente y nombre(debe coindidir con el username)
-$mail->AddAddress('armandoecr_96@hotmail.com', "Nombre Persona"); //Correo electronico  y nombre del destinatario
+$mail->AddAddress(utf8_decode($correo), utf8_decode($nombre)); //Correo electronico  y nombre del destinatario
 
-$mail->Subject = 'Pruebas PHPmailer'; //Asunto del correo electronico
-$mail->Body = 'Mensaje de prubeas PHPMailer para envio de mensajes'; //Cuerpo del correo electronico
+$mail->Subject = 'Cotizaciones'; //Asunto del correo electronico
+$mail->Body = utf8_decode($texto); //Cuerpo del correo electronico
 
 //Envio del mail
 
 $mail->send();
+
+echo($nombre);
 ?>
