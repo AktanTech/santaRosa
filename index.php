409c6d0e81d235dd55dea4d87fda4c7c9a68b7c9
GunnerStraiker1 <victorcito001@hotmail.com>
2018-02-27 16:42:55 -0600

    Commit Final v1.0

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Santa Rosa</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+
+    
+<link rel="stylesheet" href="home.css">
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="20">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                    aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="">
+                    <img src="media/imagenes/Logo-Santa-Rosa-Azul 640x494.png" alt="Santa Rosa Fabrica" width="130em">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#mivideo">Contacto</a>
+                    </li>
+                    <li>
+                        <a href="#cotizar">Cotiza con nosotros</a>
+                    </li>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <div id="mivideo" class="video-container container-flex">
+        <video class="fillWidth" autoplay loop>
+            <source src="./media/videos/SRF_Loop.mp4" type="video/mp4" /> Your browser does not support the video tag.
+        </video>
+    </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="cotizacion" role="dialog">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 class="modal-title" style="text-align:center">Cotización</h3>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <!-- Inicio del form -->
+                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" id="form-coti">
+
+                            <div class="form-group">
+                                <label for="name">Nombre</label>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Nombre">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Email address</label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="phone">Número de celular</label>
+                                <input type="text" class="form-control" name="phone" id="phone" placeholder="Télefono">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="textarea">Descripción breve de lo que necesita</label>
+                                <textarea class="form-control" name="textarea" id="textarea" rows="3"></textarea>
+                            </div>
+
+                            <!-- Fin del form -->
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                        <input type="submit" class="btn btn-primary" value="Enviar" aria-label="Close" name="submit">
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- Footer Start-->
+        <div class="container-fluid" id="footer">
+            <!--<div class="row" >-->
+            <div class="col-sm-12">
+                <h1>Contacto</h1>
+                <p style="font-size:120%">Santa Rosa Fábrica
+                    <br> Calle 42 Sur Col. Leona Vicario
+                    <br> (999) 929 87 30
+                    <br> (999) 929 43 59</p>
+                <button id="cotizar" type="button" class="btn btn-md" data-toggle="modal" data-target="#cotizacion">Cotiza con nosotros</button>
+            </div>
+            <!--</div>-->
+        </div>
+</body>
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</html>
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+//C:\xampp\htdocs\vendor\phpmailer\phpmailer\src
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+require 'vendor/autoload.php';
+//variables
+if (isset($_POST["submit"])) {
+
+
+if(isset($_POST["name"])){
+    $nombre = $_POST["name"];
+}
+
+if(isset($_POST["phone"])){
+    $celPhone = $_POST["phone"];
+}
+       
+if(isset($_POST["email"])){
+    $correo = $_POST["email"];
+}
+
+if(isset($_POST["textarea"])){
+    $texto = $_POST["textarea"];
+}
+    $mail = new PHPMailer(true);
+    try{
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; //Servidor smtp del correo
+        $mail->SMTPAuth = true;
+        $mail->Username = 'armandoecr96@gmail.com'; //Correo electronico del remitente
+        $mail->Password = 'teamoalizeebananera'; //Constraseña del remitente
+        
+        $mail->SMTPSecure = 'tls'; //Tipo de seguridad
+        $mail->Port = 587; //Puerto del servidor smtp
+        $mail->SMTPOptions = array(
+                'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        
+        //-------------------------------------------------------
+        
+        // Datos del Correo
+        
+        $mail->SetFrom('ventas@santarosa.mx', "Santa Rosa Fábrica"); //Correo electronico del remitente y nombre(debe coindidir con el username)
+        $mail->AddAddress(utf8_decode($correo), utf8_decode($nombre)); //Correo electronico  y nombre del destinatario
+        $mail->addReplyTo(utf8_decode($correo), utf8_decode($nombre));
+        
+        $mail->isHTML(true);
+        $mail->Subject = 'Cotizaciones de '. utf8_decode($nombre); //Asunto del correo electronico
+        $mail->Body = utf8_decode(
+        utf8_decode($nombre).'</br>'
+        .utf8_decode($celPhone).'</br>'
+        .utf8_decode($correo).'</br></br>'
+        . 'Me gustaría recibir una cotización de su empresa a nombre de ' . utf8_decode($nombre) .' con la siguiente descripción:</br>'
+        . utf8_decode($texto) .'</br></br>'
+        . 'De antemano muchas gracias</br>'
+        . 'Atte: '. utf8_decode($nombre));
+        //Envio del mail
+        
+        $mail->send();
+        echo '<script>
+        alert("Se ha enviado de forma correcta su mensaje");
+            </script>';
+        
+    }catch(Exception $e){
+        echo '<script>
+        alert("El mensaje no se ha enviado de forma correcta");
+        window.location.replace("index.php");
+        </script>';
+        
+    }
+}
+?>
