18701b46bf64dc264cd2d5fa281e768d86122e16
Armando Carvajal <armandoecr_96@hotmail.com>
2018-02-26 23:23:29 -0600

    Estructuración básica de envio

--- enviar.php
+++ enviar.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
-<body>
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -18,70 +9,16 @@ require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 require 'vendor/autoload.php';
 //variables
 if(isset($_POST["name"])){
-    echo 'Hola, '.$_POST["name"];
     $nombre = $_POST["name"];
-} else {
-       echo 'No has puesto tu nombre.';
-       }
+}
        
 if(isset($_POST["email"])){
-    echo 'Hola, '.$_POST["email"];
     $correo = $_POST["email"];
-} else {
-        echo 'No has puesto tu nombre.';
-        }
-
-if(isset($_POST["textarea"])){
-    echo 'Hola, '.$_POST["textarea"];
-    $texto = $_POST["textarea"];
-} else {
-        echo 'No has puesto tu nombre.';
-        }
-
-if(isset($contentHTMLAccept)){
-    $contentHTML = '<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>';
-}        
-
-if(isset($contentHTMLDen)){
-    $contentHTMLDen = '<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>';
 }
 
+if(isset($_POST["textarea"])){
+    $texto = $_POST["textarea"];
+}
 
 
 //Configuración de correo electronico
@@ -109,12 +46,19 @@ try{
     //Envio del mail
     
     $mail->send();
-    echo $contentHTMLAccept;
+    echo '<script>
+    function activar(){
+        $("#confirmPositive").modal("show");
+        {
+    </scrpt>';
 }catch(Exception $e){
-    
+    echo '<script>
+    function activar(){
+        $("#confirmNegative").modal("show");
+        {
+    </scrpt>';
 }
 ?>
-</body>
-</html>
+
 
 <!-- Inicio de codigo PHP -->
