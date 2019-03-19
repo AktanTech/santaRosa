8ec35878e3ea0b1718ab6c6192e330e9e2164c67
Armando Carvajal <armandoecr_96@hotmail.com>
2019-03-19 10:35:07 -0600

    Add reCaptcha

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <script src="gulpImage.js"></script>
     <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+    <script src='https://www.google.com/recaptcha/api.js?hl=es'></script>
 
 <link rel="stylesheet" href="home.css">
 </head>
@@ -313,7 +314,7 @@
                                 <label for="textarea">Descripción breve de lo que necesita</label>
                                 <textarea class="form-control" name="textarea" id="textarea" rows="3"></textarea>
                             </div>
-
+                            <div class="g-recaptcha" data-sitekey="6Lfbz5IUAAAAACy3RmsiU62TS31zol2KWnzRwQlX"></div>
                             <!-- Fin del form -->
                     </div>
                     <div class="modal-footer">
